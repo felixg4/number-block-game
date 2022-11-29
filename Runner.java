9593d5185d8c954cbcd3087db2660245446d9bcb
Felix Gerzon <felixg10@nycstudents.net>
2022-11-29 09:29:13 -0500

    tried to make i/o system

--- Runner.java
+++ Runner.java
@@ -5,11 +5,17 @@ public class Runner {
         Scanner scan = new Scanner(System.in);
         String userInput;
         String[] acceptableInputs = {"up", "down", "left", "right"};
+        Board gameBoard = new Board();
         do {
             System.out.print("enter a direction(\"up\"/\"down\"/\"left\"/\"right\", or \"quit\" to quit)");
             userInput = scan.nextLine();
-            if (!acceptableInputs.binarySearch("")) System.out.println("that's not a valid move!");
+            boolean goodArgs = true;
+            for (int i = 0; i < acceptableInputs.length; i++) {
+
+            }
+            if (!goodArgs) System.out.println("that's not a valid move!");
             else;
         } while (!userInput.equals("quit"));
+        scan.close();
     }
 }
