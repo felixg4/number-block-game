adcd9ad548274caca995fa7cab03a6d00d0dc02f
Felix Gerzon <teperbudget@gmail.com>
2022-12-04 20:50:23 -0500

    it works but the undo still needs to be fixed

--- Board.java
+++ Board.java
@@ -1,5 +1,6 @@
 public class Board {
     private int[][] boardArray = new int[4][4];
+    private int[][] prevBoardArray = boardArray;
     private int longest = 1;
     public Board() {
         for (int i = 0; i < (int) (Math.random() * 2) + 1; i++) {
@@ -14,6 +15,7 @@ public class Board {
     public Board(int startingNum1, int startingNum2, int startingNum1Loc, int startingNum2Loc) {
         setSquare(startingNum1Loc, startingNum2Loc);
         setSquare(startingNum2Loc, startingNum2);
+        System.out.print(this);
     }
     // make a 0 parameter method
     private int getSquare(int loc) {
@@ -27,105 +29,112 @@ public class Board {
     }
     public String toString() {
         String res = "";
-		int longestLen = (int) Math.ceil(Math.log10(longest));
+		int longestLen = (int) Math.ceil(Math.log10(longest)) + 2;
+        /*
+        String[] colorCodes = {"\033[38;2;119;110;101;48;2;238;228;218;m",
+            "\033[38;2;119;110;101;48;2;237;224;200;m",
+            "\033[38;2;249;246;242;48;2;242;177;121;m",
+            "\033[38;2;249;246;242;48;2;245;149;99;m",
+            "\033[38;2;249;246;242;48;2;246;124;95;m"
+        };
+        */
         for (int row = 0; row < 4; row++) {
-            res += "| ";
+            res += "|";
             for (int col = 0; col < 4; col++) {
                 int val = boardArray[row][col];
-                int valLength = (int) Math.ceil(Math.log10(val));
+                int valLength = (int) Math.max(Math.ceil(Math.log10(val)), 1);
                 int extraSpacing = longestLen - valLength;
+                for (int i = 0; i < extraSpacing / 2; i++) res += " ";
+                if (val > 0) res += /*colorCodes[((int) Math.ceil(Math.log10(val)/Math.log10(2)) - 1) % colorCodes.length] +*/ val;
+                else res += " ";
                 for (int i = 0; i < extraSpacing / 2 + extraSpacing % 2; i++) res += " ";
-                res += val;
-                for (int i = 0; i < extraSpacing / 2 + extraSpacing % 2; i++) res += " ";
-                res += " | ";
+                res += "|";
             }
             res += "\n";
-            for (int i = 0; i < 4*(longestLen + 4); i++) res += "-";
+            for (int i = 0; i < 4 * (longestLen +1 ); i++) res += "-";
             res += "\n";
         }
         return res;
     }
     public void move(String direction) {
-		int[] emptySpots = new int[16];
-		int j = 0;
-		for (int i = 0; i < 16; i++) {
-			if (getSquare(i) < 2) emptySpots[j++] = i;
-		}
-        // fix this logic
-        boolean canMove = false;
-        for (int row = 0; row < 4; row++) {
+        if (direction.equals("up")) {
+            prevBoardArray = boardArray;
             for (int col = 0; col < 4; col++) {
-                // can a block be combined with a block next to it?
+                int[] newCol = shiftLeft(getColumn(col));
+                for (int row = 0; row < 4; row++) boardArray[row][col] = newCol[row];
+                for (int row = 1; row < 4; row++) {
+                    int val = boardArray[row][col];
+                    if (val > 0 && boardArray[row - 1][col] == val) {
+                        boardArray[row][col] = 0;
+                        boardArray[row - 1][col] = val * 2;
+                        if (val * 2 > longest) longest = val * 2;
+                    }
+                }
+            }
+        } else if (direction.equals("down")) {
+            prevBoardArray = boardArray;
+            for (int col = 0; col < 4; col++) {
+                int[] newCol = shiftRight(getColumn(col));
+                for (int row = 0; row < 4; row++) boardArray[row][col] = newCol[row];
+                for (int row = 0; row < 3; row++) {
+                    int val = boardArray[row][col];
+                    if (val > 0 && boardArray[row + 1][col] == val) {
+                        boardArray[row][col] = 0;
+                        boardArray[row + 1][col] *= 2;
+                        if (val * 2 > longest) longest = val * 2;
+                    }
+                }
             }
         }
-		if (j == 0) {
+        else if (direction.equals("left")) {
+            prevBoardArray = boardArray;
+            for (int row = 0; row < 4; row++) {
+                boardArray[row] = shiftLeft(boardArray[row]);
+                for (int col = 1; col < 4; col++) {
+                    int val = boardArray[row][col];
+                    if (val > 0 && boardArray[row][col - 1] == val) {
+                        boardArray[row][col] = 0;
+                        boardArray[row][col - 1] *= 2;
+                        if (val * 2 > longest) longest = val * 2;
+                    }
+                }
+            }
+        } else if (direction.equals("right")) {
+            prevBoardArray = boardArray;
+            for (int row = 0; row < 4; row++) {
+                boardArray[row] = shiftRight(boardArray[row]);
+                for (int col = 2; col >= 0; col--) {
+                    int val = boardArray[row][col];
+                    if (val > 0 && boardArray[row][col + 1] == val) {
+                        boardArray[row][col] = 0;
+                        boardArray[row][col + 1] *= 2;
+                        if (val * 2 > longest) longest = val * 2;
+                    }
+                }
+            }
+        } else {
+            boardArray = prevBoardArray;
+            System.out.println(this);
+            return;
+        }
+        boolean canMatch = false;
+        for (int row = 0; row < 4 && !canMatch; row++) {
+            for (int col = 0; col < 4 && !canMatch; col++) {
+                if (!canMatch) {
+                    int val = boardArray[row][col];
+                    if ((row < 3 && val == boardArray[row + 1][col]) || (row > 0 && val == boardArray[row - 1][col])) canMatch = true;
+                    if ((col < 3 && val == boardArray[row][col + 1]) || (col > 0 && val == boardArray[row][col - 1])) canMatch = true;
+                }
+            }
+        }
+		int[] emptySpots = new int[16];
+        int j = 0;
+		for (int i = 0; i < 16; i++) if (getSquare(i) == 0) emptySpots[j++] = i;
+		if (!canMatch && j == 0) {
 			System.out.println("you lost! the biggest number you created was " + longest);
 			return;
 		}
-        // place new blocks here
-        if (direction.equals("up")) {
-            for (int col = 0; col < 4; col++) {
-                int[] newCol = shiftLeft(getColumn(col), true);
-                for (int row = 0; row < 4; row++) boardArray[row][col] = newCol[row];
-                for (int row = 3; row > 0; row--) {
-                    if (boardArray[row][col] > 0 && boardArray[row - 1][col] == boardArray[row][col]) {
-                        boardArray[row][col] = 0;
-                        boardArray[row - 1][col] *= 2;
-                        if (boardArray[row - 1][col] > longest) longest = boardArray[row - 1][col];
-                        System.out.println(this);
-                        return;
-                    }
-                }
-                newCol = shiftLeft(getColumn(col), true);
-                for (int row = 0; row < 4; row++) boardArray[row][col] = newCol[row];
-            }
-        } else if (direction.equals("down")) {
-            for (int col = 0; col < 4; col++) {
-                int[] newCol = shiftLeft(getColumn(col), false);
-                for (int row = 0; row < 4; row++) boardArray[row][col] = newCol[row];
-                for (int row = 0; row < 3; row++) {
-                    if (boardArray[row][col] > 0 && boardArray[row + 1][col] == boardArray[row][col]) {
-                        boardArray[row][col] = 0;
-                        boardArray[row + 1][col] *= 2;
-                        if (boardArray[row + 1][col] > longest) longest = boardArray[row + 1][col];
-                        System.out.println(this);
-                        return;
-                    }
-                }
-                newCol = shiftLeft(getColumn(col), false);
-                for (int row = 0; row < 4; row++) boardArray[row][col] = newCol[row];
-            }
-        } else if (direction.equals("left")) {
-            for (int row = 0; row < 4; row++) {
-                boardArray[row] = shiftLeft(boardArray[row], true);
-                for (int col = 1; col < 4; col++) {
-                    int val = boardArray[row][col];
-                    if ( boardArray[row][col] > 0 && boardArray[row][col - 1] == val) {
-                        boardArray[row][col] = 0;
-                        boardArray[row][col - 1] *= 2;
-                        if (boardArray[row][col - 1] > longest) longest = boardArray[row][col - 1];
-                        System.out.println(this);
-                        return;
-                    }
-                }
-                boardArray[row] = shiftLeft(boardArray[row], true);
-            }
-        } else {
-            for (int row = 0; row < 4; row++) {
-                boardArray[row] = shiftLeft(boardArray[row], false);
-                for (int col = 2; col >= 0; col--) {
-                    int val = boardArray[row][col];
-                    if (boardArray[row][col] > 0 && boardArray[row][col + 1] == val) {
-                        boardArray[row][col] = 0;
-                        boardArray[row][col + 1] *= 2;
-                        if (boardArray[row][col + 1] > longest) longest = boardArray[row][col + 1];
-                        System.out.println(this);
-                        return;
-                    }
-                }
-                boardArray[row] = shiftLeft(boardArray[row], false);
-            }
-        }
+        setSquare(emptySpots[(int) (Math.random() * j)], (int) Math.pow(2, (int) (Math.random() * 2) + 1));
         System.out.println(this);
     }
     private int[] getColumn(int column) {
@@ -133,20 +142,16 @@ public class Board {
         for (int i = 0; i < 4; i++) newArr[i] = boardArray[i][column];
         return newArr;
     }
-    private int[] shiftLeft(int[] arr, boolean moveRight) {
+    private int[] shiftLeft(int[] arr) {
         int[] newArr = new int[4];
         int j = 0;
-        for (int i = 0; i < 4; i++) {
-            if (arr[i] != 0) {
-                newArr[j] = arr[i];
-                j++;
-            }
-        }
-        return moveRight ? newArr : reverse(newArr);
+        for (int i = 0; i < 4; i++) if (arr[i] != 0) newArr[j++] = arr[i];
+        return newArr;
     }
-    private int[] reverse(int[] arr) {
+    private int[] shiftRight(int[] arr) {
         int[] newArr = new int[4];
-        for (int i = 0; i < 4; i++) newArr[i] = arr[3 - i];
+        int j = 3;
+        for (int i = 3; i >= 0; i--) if (arr[i] > 0) newArr[j--] = arr[i];
         return newArr;
     }
 }
--- Runner.java
+++ Runner.java
@@ -2,31 +2,33 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("welcome to 2048!");
-        /*
-        IT'S ALL BROKEN
-        IT'S ALL BROKEN
-        IT'S ALL BROKEN
-        IT'S ALL BROKEN
-        IT'S ALL BROKEN
-        IT'S ALL BROKEN
-        IT'S ALL BROKEN
-        IT'S ALL BROKEN
-        IT'S ALL BROKEN
-         */
         Scanner scan = new Scanner(System.in);
         String userInput;
-        String[] acceptableInputs = {"up", "down", "left", "right"};
-        // use wasd as well
+        String[] acceptableInputs = {"up", "down", "left", "right", "undo"};
+        String alt = "wsad";
         Board gameBoard = new Board();
+        boolean lastMoveWasUndo = false;
         do {
-            System.out.print("enter a direction(\"up\"/\"down\"/\"left\"/\"right\", or \"quit\" to quit) ");
+            System.out.print("enter a direction(\"up\"/\"down\"/\"left\"/\"right\", \"undo\" to undo, or \"quit\" to quit) ");
             userInput = scan.nextLine().toLowerCase();
             boolean goodArgs = false;
-            for (int i = 0; i < acceptableInputs.length; i++) {
-                if (!goodArgs && acceptableInputs[i].equals(userInput)) goodArgs = true;
+            for (int i = 0; i < acceptableInputs.length; i++) if (!goodArgs && acceptableInputs[i].equals(userInput)) goodArgs = true;
+            if (alt.contains(userInput) && userInput.length() == 1) {
+                goodArgs = true;
+                userInput = acceptableInputs[alt.indexOf(userInput)];
             }
             if (!goodArgs) System.out.println("that's not a valid move!");
-            else gameBoard.move(userInput);
+            else if (userInput.equals("undo")) {
+                if (lastMoveWasUndo) System.out.println("cannot undo twice in a row.");
+                else {
+                    lastMoveWasUndo = true;
+                    gameBoard.move(userInput);
+                }
+            }
+            else {
+                lastMoveWasUndo = false;
+                gameBoard.move(userInput);
+            }
         } while (!userInput.equals("quit"));
         scan.close();
     }
